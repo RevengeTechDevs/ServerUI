5e8e9c63811167f7c4034a2e74e0de140f3e1630
RevengeTechDevs <44796644+RevengeTechDevs@users.noreply.github.com>
2018-11-06 15:40:46 -0500

    Update Main.php

--- src/ServerUI/Main.php
+++ src/ServerUI/Main.php
@@ -4,6 +4,10 @@ use pocketmine\Server;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use jojoe77777\FormAPI;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+use pocketmine\event\server\ServerCommandEvent;
 class Main extends PluginBase implements Listener{
 
     public function onEnable(): void{
@@ -22,5 +26,32 @@ class Main extends PluginBase implements Listener{
             $this->getLogger()->error("ServerUI Requires FormAPI To Work");
             $this->getPluginLoader()->disablePlugin($this);
         }
+    }
+
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args):bool{
+            if($cmd->getName() == "serverui"){
+                if(!($sender instanceof Player)){
+                    $sender->sendMessage("serverui by revengetechdevs", false);
+                    return true;
+                }
+                $api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
+                $form = $api->createSimpleForm(function (Player $sender, $data){
+                    $result = $data;
+                    if ($result == null) {
+                    }
+                    switch ($result) {
+                        case 0:
+                            $sender->sendMessage("revengetchdevs serverui");
+                            break;
+                        case 1:
+                            $this->getServer()->getCommandMap()->dispatch($player, "fly");
+                            break;
+                    }
+                });
+                $form->setContent("revengetech serverui");
+                $form->addButton("Fly");
+                $form->sendToPlayer($sender);
+            }
+        return true;
     }
 }
