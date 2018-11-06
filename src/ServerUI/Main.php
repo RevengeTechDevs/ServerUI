1794b29d10cabf90b593870140eeb181c6d78b71
RevengeTechDevs <44796644+RevengeTechDevs@users.noreply.github.com>
2018-11-06 15:33:38 -0500

    Update Main.php

--- src/ServerUI/Main.php
+++ src/ServerUI/Main.php
@@ -3,6 +3,7 @@ namespace ServerUI;
 use pocketmine\Server;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
+use jojoe77777\FormAPI;
 class Main extends PluginBase implements Listener{
 
     public function onEnable(): void{
@@ -13,5 +14,13 @@ class Main extends PluginBase implements Listener{
     public function onDisable(): void{
         $this->getServer()->getPluginManager()->registerEvents(($this), $this);
         $this->getLogger()->info("Plugin Disabled");
+    }
+
+    public function checkDepends(): void{
+        $this->formapi = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
+        if(is_null($this->formapi)){
+            $this->getLogger()->error("ServerUI Requires FormAPI To Work");
+            $this->getPluginLoader()->disablePlugin($this);
+        }
     }
 }
