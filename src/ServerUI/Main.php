bd68f7dd8d6196f7da24db6acdb04c4781e72c30
RevengeTechDevs <44796644+RevengeTechDevs@users.noreply.github.com>
2018-11-06 15:13:48 -0500

    Update Main.php

--- src/ServerUI/Main.php
+++ src/ServerUI/Main.php
@@ -1,20 +1,11 @@
 <?php
-
 use pocketmine\PluginBase;
-
 use pocketmine\Server;
-
 use pocketmine\Player;
-
 class Main extends implements Listener{
 
     public function onEnable(): void{
         $this->etServer->getPluginManager()->registerEvents(($this), $this);
         $this->getLogger->()->info("Plugin Enabled");
-    }
-
-    public function onDisable(): void{
-        $this->getServer->getPluginManager()->registerEvents(($this), $this);
-        $this->getLogger->()->info("Plugin Disabled");
     }
 }
