ff674197f9404806c276bf8a4f1be34b9337654c
RevengeTechDevs <44796644+RevengeTechDevs@users.noreply.github.com>
2018-11-06 15:14:51 -0500

    typo fuck you

--- src/ServerUI/Main.php
+++ src/ServerUI/Main.php
@@ -5,7 +5,7 @@ use pocketmine\Player;
 class Main extends implements Listener{
 
     public function onEnable(): void{
-        $this->etServer->getPluginManager()->registerEvents(($this), $this);
+        $this->getServer->getPluginManager()->registerEvents(($this), $this);
         $this->getLogger->()->info("Plugin Enabled");
     }
 }
